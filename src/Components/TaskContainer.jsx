ba60f7ab6c09bf8403c2a0d355033fd30340e997
Maitra Khatri <maitrakhatri@gmail.com>
2022-05-30 00:28:54 +0530

    feat: timer and todo list

--- src/Components/TaskContainer.jsx
+++ src/Components/TaskContainer.jsx
@@ -1,12 +1,124 @@
+import { useState } from "react";
+
 export function TaskContainer() {
+
+  const [list, setList] = useState([]);
+  const [newTask, setNewTask] = useState({});
+  const [edit, setEdit] = useState(false);
+  const [toBeEdited, setToBeEdited] = useState({});
+
+  const editTodo = (item) => {
+    setToBeEdited(item);
+  };
+
+  const updateTodo = () => {
+    setList(() =>
+      list.map((item) => (item.id === toBeEdited.id ? toBeEdited : item))
+    );
+  };
+
+  const toggle = (item) => {
+    setList((todos) =>
+      todos.map((todo) =>
+        todo.id === item.id ? { ...todo, done: !item.done } : todo
+      )
+    );
+  };
+
+  const removeTask = (item) => {
+    setList((list) => list.filter((todo) => todo.id !== item.id));
+  };
+
   return (
     <div class="task-container">
-      <div class="new-task">
+      {!edit && (
+        <div className="new-task">
+          <input
+            type="text"
+            id="add-new-task"
+            placeholder="add new task"
+            value={newTask.task}
+            onChange={(e) =>
+              setNewTask({
+                id: Math.random(),
+                task: e.target.value,
+                done: false,
+              })
+            }
+          />{" "}
+          <button onClick={() => {
+            setList(() => list.concat(newTask));
+            setNewTask((todo) => {
+              return {
+                ...todo,
+                task: ""
+              }
+            })
+          }}>
+            Add
+          </button>
+        </div>
+      )}
+
+      {/* <div class="new-task">
         <input type="text" id="add-new-task" placeholder="add new task" />{" "}
         <button>Add</button>
+      </div> */}
+
+      <div className="update new-task">
+        {edit && (
+          <>
+            <input
+              type="text"
+              id="add-new-task"
+              value={toBeEdited.task}
+              onChange={(e) =>
+                setToBeEdited((item) => {
+                  return {
+                    ...item,
+                    task: e.target.value,
+                  };
+                })
+              }
+            />{" "}
+            <button
+              onClick={() => {
+                updateTodo();
+                setEdit(false);
+              }}
+            >
+              Update
+            </button>{" "}
+          </>
+        )}
       </div>
+
       <div class="task-list">
         <ol>
+          {list.map((item) => {
+            return (
+              <span class="task">
+                <li key={item.id} className={item.done ? "done" : ""}>
+                  {item.task}
+                </li>
+                <input
+                  type="checkbox"
+                  checked={item.done ? "checked" : ""}
+                  onChange={() => toggle(item)}
+                />
+                <img src="assets/edit-64.png" alt="edit"
+                  onClick={() => {
+                    editTodo(item);
+                    setEdit(true);
+                  }}
+                />{" "}
+                <img src="assets/delete.svg" alt="delete" onClick={() => removeTask(item)} />
+              </span>
+            );
+          })}
+        </ol>
+
+        {/* <ol>
           <span class="task">
             <li class="done">Design this</li>
             <input type="checkbox" />{" "}
@@ -24,7 +136,7 @@ export function TaskContainer() {
             <input type="checkbox" />{" "}
             <img src="assets/edit-64.png" alt="edit" />
           </span>
-        </ol>
+        </ol> */}
       </div>
     </div>
   );
--- src/Components/TimerContainer.jsx
+++ src/Components/TimerContainer.jsx
@@ -1,16 +1,90 @@
+import { useState } from "react";
+
 export function TimerContainer() {
-    return <div className="timer-container">
-        <div class="modes">
-            <span id="active">Focus</span>
-            <span>Short Break</span>
-            <span>Long Break</span>
-        </div>
-        <div class="timer">
-            <span>25:00</span>
-        </div>
-        <div class="actions">
-            <button id="primary">Start</button>
-            <button id="secondary">Reset</button>
-        </div>
+
+  const [secs, setSecs] = useState(0);
+  const [mins, setMins] = useState(25);
+
+  const [isPause, setIsPause] = useState(true);
+  const [reset, setReset] = useState(false);
+
+  const [focus, setFocus] = useState(true);
+  const [shortBreak, setShortBreak] = useState(false);
+  const [longBreak, setLongBreak] = useState(false);
+
+  let interval = setInterval(() => {
+    clearInterval(interval);
+    if (!reset) {
+      if (!isPause) {
+        if (secs === 0) {
+          if (mins !== 0) {
+            setSecs(59);
+            setMins(mins - 1);
+          } else {
+            //end
+          }
+        } else {
+          setSecs(secs - 1);
+        }
+      }
+    } else if (focus) {
+      setSecs(0);
+      setMins(25);
+    } else if (shortBreak) {
+      setSecs(0);
+      setMins(5);
+    } else if (longBreak) {
+      setSecs(0);
+      setMins(20);
+    }
+  }, 1000);
+
+  const startFocus = () => {
+    setShortBreak(false);
+    setLongBreak(false);
+    setFocus(true);
+    setIsPause(true);
+    setReset(true);
+  };
+
+  const startShortBreak = () => {
+    setFocus(false);
+    setLongBreak(false);
+    setShortBreak(true);
+    setIsPause(true);
+    setReset(true);
+  };
+
+  const startLongBreak = () => {
+    setFocus(false);
+    setShortBreak(false);
+    setLongBreak(true);
+    setIsPause(true);
+    setReset(true);
+  };
+
+  const timerSecs = secs < 10 ? `0${secs}` : secs;
+  const timerMins = mins < 10 ? `0${mins}` : mins;
+
+  return (
+    <div className="timer-container">
+
+      <div className="modes">
+        <span id={focus? "active" : ""} onClick={startFocus} >Focus</span>
+        <span id={shortBreak? "active" : ""} onClick={startShortBreak}>Short Break</span>
+        <span id={longBreak? "active" : ""} onClick={startLongBreak}>Long Break</span>
+      </div>
+
+      <div className="timer">
+        <span>{`${timerMins}:${timerSecs}`}</span>
+      </div>
+
+      <div className="actions">
+        <button id="primary" onClick={() => {
+          setReset(false);
+          setIsPause((isPause) => !isPause);
+        }}> {isPause ? "Start" : "Pause"} </button>
+      </div>
     </div>
+  );
 }
