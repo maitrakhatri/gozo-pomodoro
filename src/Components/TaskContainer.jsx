e3b71853fecaefaf6e795a3881c55429e8427485
Maitra Khatri <maitrakhatri@gmail.com>
2022-05-30 19:14:54 +0530

    deleted comments

--- src/Components/TaskContainer.jsx
+++ src/Components/TaskContainer.jsx
@@ -60,11 +60,6 @@ export function TaskContainer() {
         </div>
       )}
 
-      {/* <div class="new-task">
-        <input type="text" id="add-new-task" placeholder="add new task" />{" "}
-        <button>Add</button>
-      </div> */}
-
       <div className="update new-task">
         {edit && (
           <>
@@ -117,26 +112,6 @@ export function TaskContainer() {
             );
           })}
         </ol>
-
-        {/* <ol>
-          <span class="task">
-            <li class="done">Design this</li>
-            <input type="checkbox" />{" "}
-            <img src="assets/edit-64.png" alt="edit" />
-          </span>
-
-          <span class="task">
-            <li>Complete this</li>
-            <input type="checkbox" />{" "}
-            <img src="assets/edit-64.png" alt="edit" />
-          </span>
-
-          <span class="task">
-            <li>Deploy this</li>
-            <input type="checkbox" />{" "}
-            <img src="assets/edit-64.png" alt="edit" />
-          </span>
-        </ol> */}
       </div>
     </div>
   );
